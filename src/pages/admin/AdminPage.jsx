13ccc046ef7bbc2b0c1ce9629db778e449b73163
Harun Hadzic <48292553+hhadzic3@users.noreply.github.com>
2023-01-17 15:41:17 +0100

    Code refactor

--- src/pages/admin/AdminPage.jsx
+++ src/pages/admin/AdminPage.jsx
@@ -1,11 +1,52 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import axiosInstance from '../../config/axois'
-import { List } from 'antd';
+import { Table } from 'antd';
+import { Collapse } from 'antd';
+const { Panel } = Collapse;
+
+const columns = [
+  {
+    title: 'Location',
+    dataIndex: 'location',
+    key: 'location',
+  },
+  {
+    title: 'Package',
+    dataIndex: 'package',
+    key: 'package',
+  },
+  {
+    title: 'Cost',
+    dataIndex: 'cost',
+    key: 'cost',
+  },
+  {
+    title: 'Date',
+    dataIndex: 'date',
+    key: 'date',
+  },
+];
+/*
+const data = [
+  {
+    key: '1',
+    location: 'John Brown',
+    package: 'sasasa',
+    cost: 300.00,
+    date: 'date'
+  },
+];*/
 
 const AdminPage = () => {
 
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState([]);
+  const [data, setData] = useState([]);
+
+  
+  const onChange = (key) => {
+    console.log(key);
+  };
 
   useEffect(() => {
       let isMounted = true;
@@ -15,7 +56,21 @@ const AdminPage = () => {
           const response = await axiosInstance.get('/user', {
             signal: controller.signal
           })
-          isMounted && setUsers(response.data)
+          if (isMounted){ 
+            setUsers(response.data)
+            console.log(response.data);
+            const temp = [];
+            response.data.activities.forEach((activity) => {
+              temp.push({
+                key: activity.id,
+                location: activity.location.name,
+                package: activity.pack.name,
+                cost: activity.pack.cost,
+                date: activity.time
+              });
+            }) 
+            setData(temp)
+          }
         } catch (error) {
           console.log(error)
         }
@@ -30,19 +85,14 @@ const AdminPage = () => {
   },[])
 
   return (
-    <List
-    itemLayout="horizontal"
-    dataSource={users}
-    renderItem={(item) => (
-      <List.Item
-        actions={[<a key="list-loadmore-edit">Activities</a>]}
-      >
-          <List.Item.Meta
-            title={item.email}
-          />
-      </List.Item>
-    )}
-  />
+    <Collapse style={{ margin: '20px' }} defaultActiveKey={['1']} onChange={onChange}>
+      
+      {users.map(user => (
+        <Panel header={user?.email} key={user?.id}>
+          <Table columns={columns} dataSource={data} />
+        </Panel>
+      ))}
+    </Collapse>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 export { default as HomePage } from './home/HomePage';
-export { default as AuthPage } from './auth/AuthPage';
 export { default as NotFoundPage } from './notFound/NotFoundPage';
 export { default as UserPage } from './user/UserPage';
 export { default as LoginPage } from './login/LoginPage';
